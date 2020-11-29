89ef63f25fa8752787e38ed9aeba72880ff4649e
norihegyhati <75171353+norihegyhati@users.noreply.github.com>
2020-11-29 17:59:30 +0100

    Update kigyo.js

--- kigyo.js
+++ kigyo.js
@@ -4,16 +4,20 @@ const kp = 25;
 const meret = 23;
 
 let eredmeny = 0;
-let vege;
-vege = false;
 
 let kigyo = [
-    {x: Math.floor(meret / 2) * kp,
-    y: Math.floor(meret / 2) * kp},
-    {x: Math.floor(meret / 2) * kp,
-    y: Math.floor(meret / 2) * kp - kp},
-    {x: Math.floor(meret / 2) * kp,
-    y: Math.floor(meret / 2) * kp - 2 * kp}
+    {
+        x: Math.floor(meret / 2) * kp,
+        y: Math.floor(meret / 2) * kp
+    },
+    {
+        x: Math.floor(meret / 2) * kp,
+        y: Math.floor(meret / 2) * kp - kp
+    },
+    {
+        x: Math.floor(meret / 2) * kp,
+        y: Math.floor(meret / 2) * kp - 2 * kp
+    }
 ]
 for (let i = 0; i < kigyo.length; i++) {
     ctx.fillStyle = 'green';
@@ -23,22 +27,22 @@ for (let i = 0; i < kigyo.length; i++) {
 //iranyok, mozgatas
 let ir;
 ir = "UP";
-//var nyom = false;
+var nyom = false;
 document.addEventListener('keydown', irany);
 
 function irany(event) {
     if (event.keyCode == 37 && ir != 'RIGHT') {
-        ir = "LEFT";
+        ir = 'LEFT';
     }
     else if (event.keyCode == 38 && ir != 'DOWN') {
-        ir = "UP";
+        ir = 'UP';
     }
     else if (event.keyCode == 39 && ir != 'LEFT') {
-        ir = "RIGHT";
+        ir = 'RIGHT';
 
     }
     else if (event.keyCode == 40 && ir != 'UP') {
-        ir = "DOWN";
+        ir = 'DOWN';
 
     }
 }
@@ -63,20 +67,20 @@ function rajzolas() {
     let kigyoY = kigyo[0].y;
 
     if (ir == "LEFT") {
-        for(let i = 0; i < kigyo.length; i++) {
-            kigyo[0].x -= kp;   
+        for (let i = 0; i < kigyo.length; i++) {
+            kigyo[i].x -= kp;
         }
     } else if (ir == "RIGHT") {
-        for(let i = 0; i < kigyo.length; i++) {
-            kigyo[0].x += kp;
+        for (let i = 0; i < kigyo.length; i++) {
+            kigyo[i].x += kp;
         }
     } else if (ir == "UP") {
-        for(let i = 0; i < kigyo.length; i++) {
-            kigyo[0].y -= kp;
+        for (let i = 0; i < kigyo.length; i++) {
+            kigyo[i].y -= kp;
         }
     } else if (ir == "DOWN") {
-        for(let i = 0; i < kigyo.length; i++) {
-            kigyo[0].y += kp;
+        for (let i = 0; i < kigyo.length; i++) {
+            kigyo[i].y += kp;
         }
     }
 
@@ -97,17 +101,15 @@ function rajzolas() {
         y: kigyoY
     };
 
-    function collision(head, array){
-		for(let i = 0; i < array.length; i++)
-		{
-			if(head.x == array[i].x && head.y == array[i].y)
-			{
-				return true;
-			}
-		}
-		return false;    
+    function collision(head, array) {
+        for (let i = 0; i < array.length; i++) {
+            if (head.x == array[i].x && head.y == array[i].y) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     if (kigyoX < kp || kigyoY < kp ||
         collision(newHead, kigyo)) {
         clearInterval(game);
@@ -122,16 +124,6 @@ function rajzolas() {
     ctx.font = '24px Changa one';
     ctx.clearRect(0, 0, 50, 25);
     ctx.fillText(eredmeny, kp, 0.8 * kp);
-
-    
-if (vege) {
-    ctx.fillStyle = "red"
-    ctx.font = "60px Arial"
-    ctx.fillText("GAME OVER", meret / 2, meret / 2)
-    ctx.font = "20px Arial"
-    ctx.fillText("Press SPACE to start over!", meret / 2, meret / 2 + kp)
-
-  }
 
 }
 
